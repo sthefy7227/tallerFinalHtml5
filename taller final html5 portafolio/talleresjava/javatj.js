cc8f749cd8aed04190fdf9eabf4c29f971ef62d6
stefany rojas <sthefyrojas7227@gmail.com>
2023-01-21 18:04:57 -0500

    tercer punto
    portafolio con todos los talleres

---
+++ taller final html5 portafolio/talleresjava/javatj.js
@@ -0,0 +1,12 @@
+let elementosacordeon= document.getElementsByClassName("acordeonj");
+            for(let i=0;i<elementosacordeon.length;i++){
+                elementosacordeon[i].addEventListener("click",function(){
+                    this.classList.toggle("active");
+                    let panel =this.nextElementSibling;
+                    if(panel.style.display=="block"){
+                        panel.style.display="none";
+                    }else{
+                        panel.style.display="block";
+                    }
+                });
+            }
